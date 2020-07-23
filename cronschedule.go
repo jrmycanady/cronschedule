a77b61d4a568cda985401fa5089000502444448e
Jeremy Canady <jcanady@jackhenry.com>
2020-07-22 22:32:10 -0500

    Generate next value WIP

--- cronschedule.go
+++ cronschedule.go
@@ -8,6 +8,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // CronFieldValueRegex parses a single value fround in a cron field. Each field can have multiple values separated by
@@ -69,10 +70,331 @@ func (s *Schedule) PrettyString() string {
 	return prettyString
 }
 
+// ShouldExecute returns true if the schedule specifies it should execute at time t.
+func (s *Schedule) ShouldExecute(t time.Time) bool {
+	if _, ok := s.Minutes[t.Minute()]; !ok {
+		return false
+	}
+
+	if _, ok := s.Hours[t.Hour()]; !ok {
+		return false
+	}
+
+	if _, ok := s.DaysOfMonth[t.Day()]; !ok {
+		return false
+	}
+
+	if _, ok := s.Months[int(t.Month())]; !ok {
+		return false
+	}
+
+	if _, ok := s.DaysOfTheWeek[int(t.Weekday())]; !ok {
+		return false
+	}
+
+	return true
+}
+
+// ShouldExecuteNow is the same as ShouldExecute but it uses the current time.
+func (s *Schedule) ShouldExecuteNow() bool {
+	return s.ShouldExecute(time.Now())
+}
+
+func (s *Schedule) NextExecutionV3(t time.Time) time.Time {
+	year := t.Year()
+	minute := t.Minute() + 1
+	hour := t.Hour()
+	dayOfMonth := t.Day()
+	month := t.Month()
+
+	//fmt.Printf("starting time at %v\n", t)
+
+restart:
+
+	// Find next month that works by starting at the current month and rolling all the way around checking each value.
+	// Although we know all possible values for the month we don't yet know where in the list we need to start.
+	for _, ok := s.Months[int(month)]; !ok; _, ok = s.Months[int(month)] {
+		//fmt.Printf("month [%d] no good\n", month)
+		month++
+
+		if month == 13 {
+			//fmt.Printf("month exceeded, looping\n")
+			month = 1
+			year += 1
+		}
+	}
+
+	//fmt.Printf("found month %d\n", month)
+
+	// Find the next day that will work in the selected month. Each loop is limited by the number of days and possible
+	// and we need to account for leap years.
+	// Incrementing day as needed.
+	possibleDays := 0
+	switch t.Month() {
+	case time.January, time.March, time.May, time.July, time.August, time.October, time.December:
+		possibleDays = 31
+	case time.April, time.June, time.September, time.November:
+		possibleDays = 30
+	case time.February:
+		leapTime := time.Date(year, time.December, 31, 0, 0, 0, 0, time.Local)
+		if leapTime.YearDay() > 365 {
+			possibleDays = 29
+		} else {
+			possibleDays = 28
+		}
+	}
+	//fmt.Printf("month is %d with possible days %d", month, possibleDays)
+
+	// Recording start day so we can bail if we reach this again. This happens if we are looking for 31 but we are in a
+	// 28 day month.
+	startDay := dayOfMonth
+	if startDay > possibleDays {
+		//fmt.Printf("start day %d is larger than possible days %d", startDay, possibleDays)
+		dayOfMonth = 1
+		//fmt.Printf("%d-%d-%d-%d-%d", year, month, dayOfMonth, hour, minute)
+		goto restart
+	}
+
+	for {
+		_, ok := s.DaysOfMonth[dayOfMonth]
+		if ok {
+			//fmt.Printf("found good day number %d\n", dayOfMonth)
+			currentTime := time.Date(year, month, dayOfMonth, hour, minute, 0, 0, time.Local)
+			// If the day found is ont he right week day then we are good.
+			if _, ok := s.DaysOfTheWeek[int(currentTime.Weekday())]; ok {
+				break
+			}
+
+			//fmt.Printf("day is bad day of week %d\n", currentTime.Weekday())
+		}
+
+		// No good so update.
+		dayOfMonth++
+
+		// If the current date is larger than possible days then go back to the start of the month
+		if dayOfMonth > possibleDays {
+			//fmt.Printf("we are past all days in the month at %d going back to 1\n", dayOfMonth)
+			dayOfMonth = 1
+		}
+
+		if dayOfMonth == startDay {
+			//fmt.Printf("rolled back to the origial start day so rolling to a new month\n")
+			dayOfMonth = 1
+			month++
+			//fmt.Printf("%d-%d-%d-%d-%d", year, month, dayOfMonth, hour, minute)
+
+			goto restart
+		}
+	}
+
+	for _, ok := s.Hours[hour]; !ok; _, ok = s.Hours[hour] {
+		hour++
+		if hour > 23 {
+			hour = 0
+		}
+	}
+
+	for _, ok := s.Minutes[minute]; !ok; _, ok = s.Minutes[minute] {
+		minute++
+		if minute > 59 {
+			minute = 0
+		}
+	}
+
+	return time.Date(year, month, dayOfMonth, hour, minute, 0, 0, time.Local)
+}
+
+func (s *Schedule) NextExecutionsV3(t time.Time, count int) []time.Time {
+	execTimes := make([]time.Time, 0, count)
+
+	next := s.NextExecutionV3(t)
+
+	year := next.Year()
+	month := next.Month()
+	hour := next.Hour()
+	minute := next.Minute()
+	dayOfMonth := next.Day()
+	dayOfWeek := next.Weekday()
+
+	// Get slices
+	months := sortMapKeys(s.Months)
+	hours := sortMapKeys(s.Hours)
+	minutes := sortMapKeys(s.Minutes)
+	daysOfMonth := sortMapKeys(s.DaysOfMonth)
+	daysOfWeek := sortMapKeys(s.DaysOfTheWeek)
+
+	var monthsIndex int = -1
+	var monthsLen = len(months)
+
+	var hoursIndex int = -1
+	var hoursLen = len(hours)
+
+	var minutesIndex int = -1
+	var minutesLen = len(minutes)
+
+	var daysOfMonthIndex int = -1
+	var daysOfMonthLen = len(daysOfMonth)
+
+	var daysOfWeekIndex int = -1
+	//var daysOfWeekLen = len(s.DaysOfTheWeek)
+
+	for i, v := range months {
+		if int(month) == v {
+			monthsIndex = i
+			break
+		}
+	}
+	if monthsIndex == -1 {
+		panic("months index not found")
+	}
+
+	for i, v := range hours {
+		if hour == v {
+			hoursIndex = i
+			break
+		}
+	}
+	if hoursIndex == -1 {
+		panic("hoursIndex not found")
+	}
+
+	for i, v := range minutes {
+		if minute == v {
+			minutesIndex = i
+			break
+		}
+	}
+	if minutesIndex == -1 {
+		panic("minutesIndex not found")
+	}
+
+	for i, v := range daysOfMonth {
+		if dayOfMonth == v {
+			daysOfMonthIndex = i
+			break
+		}
+	}
+	if daysOfMonthIndex == -1 {
+		panic("daysOfMonth not found")
+	}
+
+	for i, v := range daysOfWeek {
+		if int(dayOfWeek) == v {
+			daysOfWeekIndex = i
+			break
+		}
+	}
+	if daysOfWeekIndex == -1 {
+		panic("daysOfWeekIndex not found")
+	}
+
+	foundCount := 0
+
+	for foundCount != count {
+
+		for monthsIndex < monthsLen {
+			month := months[monthsIndex]
+
+			for daysOfMonthIndex < daysOfMonthLen {
+				day := daysOfMonth[daysOfMonthIndex]
+
+				possibleDays := 0
+				switch time.Month(month) {
+				case time.January, time.March, time.May, time.July, time.August, time.October, time.December:
+					possibleDays = 31
+				case time.April, time.June, time.September, time.November:
+					possibleDays = 30
+				case time.February:
+					leapTime := time.Date(year, time.December, 31, 0, 0, 0, 0, time.Local)
+					if leapTime.YearDay() > 365 {
+						possibleDays = 29
+					} else {
+						possibleDays = 28
+					}
+				}
+
+				// If past possible days of the month bail.
+				if day > possibleDays {
+					break
+				}
+
+				// Now that we have the day go ahead and validate the day of the week.
+				weekdayT := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.Local)
+				if _, ok := s.DaysOfTheWeek[int(weekdayT.Weekday())]; ok {
+
+					for hoursIndex < hoursLen {
+						hour := hours[hoursIndex]
+						fmt.Printf("%v", s.Hours)
+						fmt.Printf("hour: %v", hour)
+						for minutesIndex < minutesLen {
+							minute := minutes[minutesIndex]
+
+							execT := time.Date(year, time.Month(month), day, hour, minute, 0, 0, time.Local)
+							execTimes = append(execTimes, execT)
+							foundCount++
+
+							minutesIndex++
+
+						}
+
+						minutesIndex = 0
+						hoursIndex++
+					}
+
+					hoursIndex = 0
+				}
+
+				daysOfMonthIndex++
+			}
+
+			daysOfMonthIndex = 0
+			monthsIndex++
+		}
+
+		// Exhausted months left, resetting to next year.
+		monthsIndex = 0
+		year++
+
+	}
+
+	return execTimes
+}
+
+// NextExecutionV2
+// Test month?
+// Test day
+// Test day of week
+// Test Hour
+// Test Minute
+
+// NextExecution returns the time when the next execution should happen after the time provides.
+func (s *Schedule) NextExecution(t time.Time) time.Time {
+	t = t.Add(1 * time.Minute)
+
+	found := s.ShouldExecute(t)
+	for !found {
+		t = t.Add(1 * time.Minute)
+		found = s.ShouldExecute(t)
+	}
+
+	return t
+}
+
+// NextExecutions returns a list (count) next execution times for the schedule.
+func (s *Schedule) NextExecutions(t time.Time, count int) []time.Time {
+	executionTimes := make([]time.Time, 0, 0)
+	for i := 0; i < count; i++ {
+		nextT := s.NextExecution(t)
+		executionTimes = append(executionTimes, s.NextExecution(t))
+		t = nextT
+	}
+	return executionTimes
+}
+
 // sortMapKeys sorts the keys of an int keyed map and returns a slice of the sorted keys.
 func sortMapKeys(m map[int]int) []int {
 	list := make([]int, 0, len(m))
-	for k, _ := range m {
+	for k := range m {
 		list = append(list, k)
 	}
 	sort.Ints(list)
--- cronschedule_test.go
+++ cronschedule_test.go
@@ -4,6 +4,7 @@ import (
 	"cronschedule"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestCronSchedule(t *testing.T) {
@@ -15,5 +16,65 @@ func TestCronSchedule(t *testing.T) {
 	}
 
 	fmt.Println(schedule.PrettyString())
+
+}
+
+func TestNextExecutionTimes(t *testing.T) {
+	scheduleStr := "1,2,3,4,5 1 23 1 1"
+	schedule, err := cronschedule.Parse(scheduleStr)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	execTimes := schedule.NextExecutions(time.Now(), 20)
+
+	for _, t := range execTimes {
+		fmt.Println(t)
+	}
+}
+
+func TestNextExecutionTimesV3(t *testing.T) {
+	scheduleStr := "1,2,3,4,5 1 23 1 1"
+	schedule, err := cronschedule.Parse(scheduleStr)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	execTimes := schedule.NextExecutionsV3(time.Now(), 20)
+
+	for _, t := range execTimes {
+		fmt.Println(t)
+	}
+}
+
+func TestV1vsV3(t *testing.T) {
+	scheduleStr := "0 1 23 1 1"
+	schedule, err := cronschedule.Parse(scheduleStr)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	execTimes := schedule.NextExecutionsV3(time.Now(), 5)
+
+	for _, t := range execTimes {
+		fmt.Println(t)
+	}
+
+	execTimes = schedule.NextExecutions(time.Now(), 5)
+
+	for _, t := range execTimes {
+		fmt.Println(t)
+	}
+}
+
+func TestNextExecutionV3Times(t *testing.T) {
+	scheduleStr := "0 1 23 1 1"
+	schedule, err := cronschedule.Parse(scheduleStr)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	execTimes := schedule.NextExecutionV3(time.Now())
+	fmt.Println(execTimes)
 
 }
